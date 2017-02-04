3c93db3027d9c58ba632d96aa21cc4211075fdaf
horbachevsky vladislav <horbachevsky vladislav>
2017-02-04 03:01:19 +0300

    all methods are implemented, 3 tests failing

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,75 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (!config) {
+            throw new Error("config isn't passed");
+        } else {
+            this.state = config.initial;
+            this.config = config;
+            //this.prevState = null;
+            //this.nextState = null;
+            this.flagForChangeState = null;
+            this.flagForTrigger = null;
+            this.count = 0;
+            this.storageOfStates = [];
+        }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        this.count ++;
+        this.storageOfStates[this.count] = this.state;
+        this.flagForChangeState = false;
+        if (!this.config.states[state]) {
+            throw new Error("such state isn't exist");
+        } else {
+            this.prevState = this.state;
+            this.state = state;
+            this.count ++;
+            this.storageOfStates[this.count] = this.state;
+        }
+    }
+
+
+    //console.log(config.states.busy.transitions.get_hungry);
+    //console.log(config["states"]["busy"]["transitions"]["get_hungry"]);
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        this.count ++;
+        this.storageOfStates[this.count] = this.state;
+        this.flagForTrigger = false;
+        var newState = this.config.states[this.state].transitions[event];
+        if (!newState) {
+            throw new Error("such event isn't exist");
+        }
+        this.prevState = this.state;
+        this.state = newState;
+        this.count ++;
+        this.storageOfStates[this.count] = this.state;
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.state = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +79,68 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+
+        var states = Object.keys(this.config.states);
+        var statesOfEvent = [];
+
+        if (!event) {
+            return states;
+        }
+        for (var i = 0; i < states.length; i ++) {
+            var transitions = this.config.states[states[i]].transitions;
+            if (transitions[event]) {
+                statesOfEvent.push(states[i]);
+            }
+        }
+        this.count ++;
+        this.storageOfStates[this.count] = this.state;
+        return statesOfEvent;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        this.flagForChangeState = true;
+        this.flagForTrigger = true;
+
+        if (this.state == this.config.initial) {
+            return false;
+        } else if ((this.count --) && (this.state = this.storageOfStates[this.count])) {
+            return true;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        this.count ++;
+        this.state = this.storageOfStates[this.count];
+        if (this.flagForChangeState) {
+            return false;
+        } else if (this.flagForTrigger) {
+            return false;
+        } else if (this.state == this.config.initial) {
+            return false;
+        } else if ((this.count ++) && (this.state = this.storageOfStates[this.count])) {
+            return true;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.storageOfStates = [];
+        //this.config.initial = 'normal';
+
+    }
 }
 
 module.exports = FSM;
